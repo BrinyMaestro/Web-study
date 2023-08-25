1fb829252c3347bca67551e72791837aa11e75dd
BrinyMaestro <127175337+BrinyMaestro@users.noreply.github.com>
2023-08-25 11:14:38 +0900

    NodeJsProject

--- NodeJsProject/app.js
+++ NodeJsProject/app.js
@@ -4,8 +4,11 @@ const app = express()
 const helmet = require("helmet") // 보안을 위한 것
 app.use(helmet()) // app.use() : express에서 미들웨어를 등록하고 사용하는 함수.
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 const mainRouter = require("./router/mainRouter.js")
-app.use("/aicom", mainRouter)
+app.use("/", mainRouter)
 
 app.listen(3000, function() {
     console.log('hello')
--- NodeJsProject/router/mainRouter.js
+++ NodeJsProject/router/mainRouter.js
@@ -6,7 +6,14 @@ router.get('/', function(req, res) {
 })
 
 router.get('/about', function(req, res) {
-    res.send("오와캬퍄헉농쭉튼빵뿅ㅋㅋ")
+    res.send(`이름은 ${req.query.name}이고 종족은 ${req.query.race}입니다`)
+    console.log(req.query.name)
+    console.log(req.query.race)
+
 })
 
+router.post("/about", function(req, res) {
+    console.log("포스트실행")
+    console.log(req.body)
+})
 module.exports = router
